b837f64f3e590efd97fb36d1d6fb4d108c251f02
WillGoad <goad.w@etoncollege.org.uk>
2019-11-05 12:16:16 +0000

    Initial Commit of Project Files

---
+++ StandardAlgorithm/main.swift
@@ -0,0 +1,19 @@
+//
+//  main.swift
+//  StandardAlgorithm
+//
+//  Created by Goad, William (PAH) on 05/11/2019.
+//  Copyright © 2019 Goad, William (PAH). All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
+let name: String = "Will Goad"
+
+for i in 1...3 {
+    print(i)
+}
+
+print(name)
